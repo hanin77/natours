335b21518226768c4048c2933435df169f4a6f70
hanin77 <mohammadhanin77@gmail.com>
2020-12-01 19:46:00 +0100

    added middlewares rate limmiting, helmet,mongo-sanitize, xss, hpp, and limited the payload size in json middleware to 10kb

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 const morgan = require('morgan');
+const hpp = require('hpp');
 const path = require('path');
 const userRouter = require('./routes/userRoutes');
 const tourRouter = require('./routes/tourRoutes');
@@ -9,16 +14,44 @@ const globalErrorHandler = require('./controllers/errorController');
 const publicPath = path.join(__dirname, 'public');
 
 const app = express();
-//middlewares
+//middlewares//
+//set security headers
+app.use(helmet());
+//dev middlwares
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-app.use(express.json());
+// we limit number of request from one ip to 100 req per hour
+const Limitter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in a hour'
+});
+app.use('/api', Limitter);
+//Body parser reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+//Data sanitisation against Nosql query injection
+app.use(mongoSanitize());
+//Data sanitisation against xss
+app.use(xss());
+//Prevent parammeter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingQuantity',
+      'ratingAverage',
+      'difficulty',
+      'price'
+    ]
+  })
+);
 app.use(express.static(publicPath));
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
+//test middlware
 // app.use((req, res, next) => {
 //   console.log('hi from middleware');
 //   next();
