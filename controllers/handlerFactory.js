d5f73ec8e1265eb6b3360edaf4c8e93631d40f16
hanin77 <mohammadhanin77@gmail.com>
2020-12-08 01:45:08 +0100

    added factory functions deleteOne, updateOne, createOne, getOne, getAll

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,4 +1,5 @@
 const AppError = require('./../utils/appError');
+const APIFeatures = require('./../utils/APIFeatures');
 const catchAsync = require('./../utils/catchAsync');
 
 exports.deleteOne = Model => {
@@ -10,6 +11,73 @@ exports.deleteOne = Model => {
     res.status(204).json({
       status: 'success',
       data: null
+    });
+  });
+};
+exports.updateOne = Model => {
+  return catchAsync(async (req, res, next) => {
+    const updatedDoc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+    if (!updatedDoc) {
+      return next(new AppError('No doc found with provided ID', 404));
+    }
+    res.status(200).json({
+      status: 'success',
+      data: updatedDoc
+    });
+  });
+};
+
+exports.createOne = Model => {
+  return catchAsync(async (req, res, next) => {
+    const newDoc = await Model.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        data: newDoc
+      }
+    });
+  });
+};
+
+exports.getOne = (Model, populateOptions) => {
+  return catchAsync(async (req, res, next) => {
+    let query = Model.findById(req.params.id);
+    if (populateOptions) query = query.populate(populateOptions);
+    const doc = await query;
+    if (!doc) {
+      return next(new AppError('No document found with provided ID', 404));
+    }
+    res.status(200).json({
+      status: 'success',
+      data: doc
+    });
+  });
+};
+
+exports.getAll = Model => {
+  return catchAsync(async (req, res, next) => {
+    //to allow for nested GET reviews tour
+    let filter = {};
+    if (req.params.tourId) filter = { tour: req.params.tourId };
+    //filter reserved word
+    //sort
+    //Field limiting
+    //pagination
+    //execute query
+    const features = new APIFeatures(Model.find(filter), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const docs = await features.query;
+    res.status(200).json({
+      status: 'success',
+      results: docs.length,
+      data: { docs }
     });
   });
 };
--- controllers/userController.js
+++ controllers/userController.js
@@ -48,29 +48,10 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getAllUsers = catchAsync(async (req, res) => {
-  const users = await User.find();
-  res.status(200).json({
-    status: 'success',
-    results: users.length,
-    data: {
-      users
-    }
-  });
-});
+exports.getAllUsers = factory.getAll(User);
 
-exports.getUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'this route is not yet defined!'
-  });
-};
+exports.getUser = factory.getOne(User);
 
-exports.updateUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'this route is not yet defined!'
-  });
-};
+exports.updateUser = factory.updateOne(User);
 
 exports.deleteUser = factory.deleteOne(User);
