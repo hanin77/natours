9c4eda6e2be3474602af9a4e7cd8491b1ff62d16
hanin77 <mohammadhanin77@gmail.com>
2020-12-12 19:50:08 +0100

    implemented the tour template view

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -3,15 +3,19 @@ const catchAsync = require('../utils/catchAsync');
 
 exports.getOverview = catchAsync(async (req, res, next) => {
   const tours = await Tour.find();
-  console.log(tours);
   res.status(200).render('overview', {
     title: 'All tours',
     tours: tours
   });
 });
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'dump tour title'
+exports.getTour = catchAsync(async (req, res) => {
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user'
   });
-};
+  res.status(200).render('tour', {
+    title: tour.name,
+    tour: tour
+  });
+});
