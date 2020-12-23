f9b803dd6701b50313fd42488e440a7682976afc
hanin77 <mohammadhanin77@gmail.com>
2020-12-23 14:57:36 +0100

    removed the catchAsync function

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -49,12 +49,12 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 //   res.redirect(req.originalUrl.split('?')[0]);
 // });
 
-const createBookingCheckout = catchAsync(async session => {
+const createBookingCheckout = async session => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.display_items[0].amount / 100;
   await Booking.create({ tour, user, price });
-});
+};
 
 exports.webhookCheckout = (req, res, next) => {
   const signature = req.headers['stripe-signature'];
